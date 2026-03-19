11170e497c34bef69b78bac2aa8df25b3baf59f3
Oliver Gudenschwager <osansyp@gmail.com>
2026-03-19 12:07:48 +0100

    Fix: Naming conventions Fix: Architecture

--- controllers/History.js
+++
@@ -1,7 +0,0 @@
-class History
-{
-    update(I_temperature)
-    {
-        document.getElementById("tabpanel-2").appendChild(document.createElement("p")).textContent = I_temperature + "°C";
-    }
-}
--- index.php
+++ index.php
@@ -3,32 +3,32 @@
     <title>Capteur</title>
 </head>
 <body>
-    <h1>Température</h1>
-    <div class="tabs">
-        <div role="tablist" aria-labelledby="tablist-1" class="manual">
-            <button id="tab-1" type="button" role="tab" aria-selected="true" aria-controls="tabpanel-1">
-                <span class="focus">Actuelle</span>
-            </button>
-            <button id="tab-2" type="button" role="tab" aria-selected="false" aria-controls="tabpanel-2" tabindex="-1">
-                <span class="focus">Historique</span>
-            </button>
-        </div>
-
-        <div id="tabpanel-1" role="tabpanel" aria-labelledby="tab-1">
-            <section>
-                <p id="value" aria-live="assertive"></p>
-                <p id="message" role="alert"></p>
-            </section>
-        </div>
-        <div id="tabpanel-2" role="tabpanel" aria-labelledby="tab-2" class="is-hidden">
-
-        </div>
+<h1>Température</h1>
+<div class="tabs">
+    <div role="tablist" aria-labelledby="tablist-1" class="manual">
+        <button id="tab-1" type="button" role="tab" aria-selected="true" aria-controls="tabpanel-1">
+            <span class="focus">Actuelle</span>
+        </button>
+        <button id="tab-2" type="button" role="tab" aria-selected="false" aria-controls="tabpanel-2" tabindex="-1">
+            <span class="focus">Historique</span>
+        </button>
     </div>
 
-    <script src="/models/Temperature.js"></script>
-    <script src="/controllers/Display.js"></script>
-    <script src="/controllers/History.js"></script>
-    <script src="/controllers/Alert.js"></script>
-    <script src="/Main.js" ></script>
-    <script src="/controllers/TabsManual.js" ></script>
+    <div id="tabpanel-1" role="tabpanel" aria-labelledby="tab-1">
+        <section>
+            <p id="value" aria-live="assertive"></p>
+            <p id="message" role="alert"></p>
+        </section>
+    </div>
+    <div id="tabpanel-2" role="tabpanel" aria-labelledby="tab-2" class="is-hidden">
+
+    </div>
+</div>
+
+<script src="/models/Temperature.js"></script>
+<script src="/controllers/Display.js"></script>
+<script src="/controllers/History.js"></script>
+<script src="/controllers/Alert.js"></script>
+<script src="/Main.js" ></script>
+<script src="/controllers/TabsManual.js" ></script>
 </body>
