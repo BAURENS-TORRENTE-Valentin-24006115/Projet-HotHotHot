3e7bc650beaf373ac0048c5cb0a7e54e62fc7c8c
DumontALINE <aline.dmnt83@gmail.com>
2026-03-26 11:28:01 +0100

    fix : amélioration du css pour les toast

--- controllers/Alert.js
+++ controllers/Alert.js
@@ -11,9 +11,10 @@ function creatNotificatio(S_message){
     }
 }
 
-function creatToast(S_message) {
+function creatToast(S_message, S_type) {
     let O_toast = document.createElement("div");
     O_toast.classList.add("toast");
+    O_toast.classList.add(S_type);
     O_toast.textContent = S_message;
 
     document.getElementById("toast-container").appendChild(O_toast)
@@ -29,11 +30,11 @@ class Alert {
         if (I_temperature < 0) {
             S_message = "Brrrrrrr, un peu froid ce matin, mets ta cagoule !";
             creatNotificatio(S_message);
-            creatToast(S_message);
+            creatToast(S_message, "toast-froid");
         } else if (I_temperature > 30) {
             S_message = "Caliente ! Vamos a la playa, ho hoho hoho !!";
             creatNotificatio(S_message);
-            creatToast(S_message);
+            creatToast(S_message, "toast-chaud");
         }
 
         document.getElementById("message").textContent = S_message;
